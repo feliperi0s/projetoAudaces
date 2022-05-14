035f8659c1d53894d2985e486d410a2848f1c39c
feliperi0s <94755355+feliperi0s@users.noreply.github.com>
2022-05-14 12:08:11 -0300

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { EsqueciSenhaComponent } from './components/esqueci-senha/esqueci-senha.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EsqueciSenhaComponent
   ],
   imports: [
     BrowserModule,

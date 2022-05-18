8d6a2ef3fd65b1107c56510d1af5f8636bdb90b3
feliperi0s <94755355+feliperi0s@users.noreply.github.com>
2022-05-18 17:56:55 -0300

    serviço

--- src/app/components/colecao-form/colecao-form.component.ts
+++ src/app/components/colecao-form/colecao-form.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Colecao } from 'src/app/_interfaces/colecao';
+import { ColecaoService } from 'src/app/_servicos/colecao.service';
+import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-colecao-form',
@@ -9,22 +15,76 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ColecaoFormComponent implements OnInit {
 
   public form!:FormGroup
+  public submitted = false
+  public colecao$!:Observable<Colecao[]>
 
-  constructor(private _formBuilder:FormBuilder) { }
+  constructor(private _formBuilder:FormBuilder, private _colecaoService :ColecaoService, private _route:ActivatedRoute, private _location :Location) { }
 
   ngOnInit(): void {
+    this.colecao$ = this._colecaoService.getColecao()
+    let registro = null
     this.form = this._formBuilder.group({
+      id: null,
       nome:["",[Validators.required]],
+      estacao:["",[Validators.required]],
       responsavel:["",[Validators.required]],
-      tipo:["",[Validators.required]],
-      colecao:["",[Validators.required]],
-      bordado:[false,[Validators.required]],
-      estampa:[false,[Validators.required]]
-    })
+      marca:["",[Validators.required]],
+      anoLancamento:[false,[Validators.required]],
+      orcamento:[false,[Validators.required]]
+    });
+    this._route.params.subscribe(
+      (params: any) => {
+        const id = params['id'];
+         const colecao$ = this._colecaoService.loadById(id);
+         colecao$.subscribe( colecao => {
+           registro = colecao;
+           this.updateForm(colecao)
+         }
+
+         )
+      }
+    )
   }
 
   public onSubmit(){
-    console.log(this.form.value)
+    
+    this.submitted = true
+    if(this.form.valid){
+      
+      if(this.form.value.id){
+        this._colecaoService.update(this.form.value).subscribe(
+          success => console.log('sucess'),
+          () => console.log('erro')
+        )
+        this._location.back()
+      }else {
+        this._colecaoService.postModelos(this.form.value).subscribe()
+        this._location.back()
+      }
+    }  
   }
+
+  public updateForm(colecao:any){
+    this.form.patchValue({
+      id : colecao.id,
+      nome: colecao.nome,
+      estacao: colecao.estacao,
+      responsavel:colecao.responsavel,
+      marca: colecao.marca,
+      anoLancamento: colecao.anoLancamento,
+      orcamento: colecao.orcamento
+    })
+  }
+
+  public onCancel(){
+    this.submitted = false;
+    if(this.form.value.id){
+      this._location.back()
+    }else {
+      this.form.reset();
+      this._location.back()
+    }
+  }
+
 
 }
